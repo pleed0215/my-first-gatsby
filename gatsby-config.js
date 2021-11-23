e83fd9878f0478f41be0bf12273e834868ae6336
pleed0215 <pleed0215@gmail.com>
2021-11-23 15:50:59 +0900

    Tutorial step7

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,6 @@ module.exports = {
             },
         },
         'gatsby-plugin-mdx',
+        'gatsby-transformer-sharp',
     ],
 };
--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import Layout from '../../components/Layout';
 import { graphql, PageProps } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const BlogPost: React.FC<PageProps> = ({ data }: { data: any }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image);
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage
+                alt={data.mdx.frontmatter.hero_image_alt}
+                image={image}
+            />
             <MDXRenderer>{data.mdx.body}</MDXRenderer>
         </Layout>
     );
@@ -20,6 +26,14 @@ export const query = graphql`
             frontmatter {
                 title
                 date(formatString: "YYYY-MM-DD")
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
             body
         }
